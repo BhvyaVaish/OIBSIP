838f1611f4e878affe3c58b127b95ea6b5066091
Bhvya Vaish <Bhvya.vaish07@gmail.com>
2026-06-25 13:18:52 +0530

    Deployment Setup

--- Task_3_Car_Price_Prediction/app.py
+++ Task_3_Car_Price_Prediction/app.py
@@ -49,5 +49,8 @@ def predict():
         seller_type=seller_type,
         transmission=transmission
     )
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
